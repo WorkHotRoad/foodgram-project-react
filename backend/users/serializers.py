a94f95869162c219173b217b1fce244283ac042e
Дмитрий <dima@ezoport.com>
2022-11-14 00:01:36 +0300

    add validate in serialazers

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,10 +2,11 @@
 from drf_extra_fields.fields import Base64ImageField
 
 from djoser.serializers import UserCreateSerializer
+from django.shortcuts import get_object_or_404
 from recipe.models import Recipe
 from rest_framework import serializers
 
-from .models import Follow
+from .models import Follow, User
 
 
 class CustomUserCreateSerializer(UserCreateSerializer):
@@ -29,12 +30,10 @@ class UserSerializer(UserCreateSerializer):
         )
 
     def get_is_subscribed(self, obj):
-        user = self.context.get('request').user
-        if user.is_authenticated:
-            return Follow.objects.filter(
-                user=user, following=obj.id
-            ).exists()
-        return False
+        request = self.context['request']
+        if request.user.is_anonymous:
+            return False
+        return Follow.objects.filter(user=request.user, following=obj).exists()
 
 
 class FavoritRecipeSerializer(serializers.ModelSerializer):
@@ -45,39 +44,52 @@ class FavoritRecipeSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'image', 'cooking_time')
 
 
-class FollowSerializer(serializers.ModelSerializer):
-    id = serializers.ReadOnlyField(source='following.id')
-    email = serializers.ReadOnlyField(source='following.email')
-    username = serializers.ReadOnlyField(source='following.username')
-    first_name = serializers.ReadOnlyField(source='following.first_name')
-    last_name = serializers.ReadOnlyField(source='following.last_name')
-    is_subscribed = serializers.SerializerMethodField()
+class ShowFollowsSerializer(UserSerializer):
     recipes = serializers.SerializerMethodField()
     recipes_count = serializers.SerializerMethodField()
 
     class Meta:
-        model = Follow
-        fields = (
-            'email', 'id', 'username',
-            'first_name', 'last_name',
-            'is_subscribed', 'recipes',
-            'recipes_count'
-        )
-
-    def get_is_subscribed(self, obj):
-        return Follow.objects.filter(
-            user=obj.user, following=obj.following
-        ).exists()
+        model = User
+        fields = ('email', 'id', 'username', 'first_name',
+                  'last_name', 'is_subscribed', 'recipes', 'recipes_count')
 
     def get_recipes(self, obj):
-        request = self.context.get('request')
+        queryset = Recipe.objects.filter(author=obj)
+        request = self.context['request']
         limit = request.GET.get('recipes_limit')
-        queryset = Recipe.objects.filter(author=obj.following)
         if limit:
             queryset = queryset[:int(limit)]
         return FavoritRecipeSerializer(queryset, many=True).data
-        # recipe = Recipe.objects.filter(author=obj.following)
-        # return FavoritRecipeSerializer(recipe, many=True).data
 
     def get_recipes_count(self, obj):
-        return Recipe.objects.filter(author=obj.following).count()
+        queryset = Recipe.objects.filter(author=obj)
+        return queryset.count()
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.IntegerField(source='user.id')
+    following = serializers.IntegerField(source='following.id')
+
+    class Meta:
+        model = Follow
+        fields = ['user', 'following']
+
+    def validate(self, data):
+        user = data['user']['id']
+        following = data['following']['id']
+        follow_exist = Follow.objects.filter(
+            user=user, following__id=following
+        ).exists()
+        if user == following:
+            raise serializers.ValidationError(
+                {"errors": 'Вы не можете подписаться на самого себя'}
+            )
+        elif follow_exist:
+            raise serializers.ValidationError({"errors": 'Вы уже подписаны'})
+        return data
+
+    def create(self, validated_data):
+        following = validated_data.get('following')
+        following = get_object_or_404(User, pk=following.get('id'))
+        user = validated_data.get('user')
+        return Follow.objects.create(user=user, following=following)
--- backend/users/views.py
+++ backend/users/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 
 from .models import Follow, User
 from .pagination import LimitPageNumberPagination
-from .serializers import FollowSerializer
+from .serializers import FollowSerializer, ShowFollowsSerializer
 
 
 class FollowUserViewSet(UserViewSet):
@@ -18,46 +18,37 @@ class FollowUserViewSet(UserViewSet):
         methods=['post', 'delete']
     )
     def subscribe(self, request, id=None):
-        user = request.user
         following = get_object_or_404(User, id=id)
+        serializer = FollowSerializer(
+            data={'user': request.user.id, 'following': id}
+        )
         if request.method == 'POST':
-            if user == following:
-                return Response({
-                    'errors': 'Вы не можете подписываться на самого себя'
-                }, status=status.HTTP_400_BAD_REQUEST)
-            if Follow.objects.filter(user=user, following=following).exists():
-                return Response({
-                    'errors': f'Вы уже подписаны на {following.username}'
-                }, status=status.HTTP_400_BAD_REQUEST)
-            follow = Follow.objects.create(user=user, following=following)
-            serializer = FollowSerializer(
-                follow,
-                context={'request': request}
+            serializer.is_valid(raise_exception=True)
+            serializer.save(user=request.user)
+            serializer = ShowFollowsSerializer(
+                following, context={'request': self.request}
             )
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         if request.method == 'DELETE':
-            follow = Follow.objects.filter(user=user, following=following)
-            if follow.exists():
-                follow.delete()
-                return Response(
-                    {f'Вы отписались от {following.username}'},
-                    status=status.HTTP_204_NO_CONTENT
-                )
-            return Response({
-                'Вы уже отписались'
-            }, status=status.HTTP_400_BAD_REQUEST)
-        return None
+            follow = get_object_or_404(
+                Follow, user=request.user, following__id=id
+            )
+            follow.delete()
+            return Response(
+                f'ВЫ отписались от {follow.following}',
+                status=status.HTTP_204_NO_CONTENT
+            )
+        return Response(
+            status=status.HTTP_400_BAD_REQUEST)
 
     @action(
         detail=False, permission_classes=[permissions.IsAuthenticated],
         methods=['get']
     )
     def subscriptions(self, request):
-        user = request.user
-        queryset = Follow.objects.filter(user=user)
-        pages = self.paginate_queryset(queryset)
-        # pages = self.paginate_queryset(follow)
-        serializer = FollowSerializer(
+        user = User.objects.filter(following__user=request.user)
+        pages = self.paginate_queryset(user)
+        serializer = ShowFollowsSerializer(
             pages,
             many=True,
             context={'request': request}
