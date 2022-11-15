9e9ea407e046703d525c8c4c3aea494faa3c43ed
Дмитрий <dima@ezoport.com>
2022-11-16 00:46:57 +0300

    workflow03

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,9 +6,9 @@ load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['51.250.28.59']
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
